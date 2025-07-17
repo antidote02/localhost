68ea621e2298f00115965ea2ac950f7a64eb6d49
antidote02 <a1729304580@gmail.com>
2025-07-17 12:04:09 +0800

    2025-7-17 12:04:15

--- ffmpeg/bat/vbr_cq.bat
+++ ffmpeg/bat/vbr_cq.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set "input=%~1"
-set "cq=34"
+set "cq=35"
 set "output=%~dp1\%~n1_%cq%.mp4"
 ffmpeg -hide_banner -i "%input%" -c:v hevc_nvenc -preset p7 -profile:v main -s 1280x720 -r 60000/1001 -rc vbr -tune hq -cq %cq% -tier main -c:a copy "%output%"
 pause
