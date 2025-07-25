58e706836bb28839d1d856a6575b0796223c11a4
antidote02 <a1729304580@gmail.com>
2025-07-25 19:53:12 +0800

    2025-7-25 19:53:14

---
+++ ffmpeg/bat/copy.bat
@@ -0,0 +1,2 @@
+ffmpeg -hide_banner -i "%~1" -c copy "%~1_2.mp4"
+pause
--- ffmpeg/bat/jable.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set "input=%~1"
-set "cq=35"
-set "output=%~dp1\%~n1_%cq%.mp4"
-ffmpeg -hide_banner -i "%input%" -c:v hevc_nvenc -preset p7 -profile:v main -tune hq -s 1280x720 -r 60000/1001 -rc vbr -cq %cq% -tier main -c:a copy "%output%"
-pause
